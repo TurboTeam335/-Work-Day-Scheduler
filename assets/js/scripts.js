239f0e202763fb18d532a20caec82465d321f572
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-03-24 16:20:17 -0700

    calendar color issue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,62 +14,62 @@ $(document).ready(function () {
   $('.saveBtn').on('click', function(){
     var text = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
-
+    
     localStorage.setItem(time, text);
-
+    
     $('notification').addClass('show');
-
-    setTimeout(function (){
-      $('.notification').removeClass('show');
-    }, 5000);
+    
+    // setTimeout(function (){
+    //   $('.notification').removeClass('show');
+    // }, 5000);
   })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-    function hourUpdater () {
-      var currentHour = dayjs().hour();
+  function workHour () {
+    var currentHour = dayjs().hour();
+    
+    $('.time-block').each(function(){
+      var otherHour = parseInt($(this).attr('id').split('-')[1]);
+      
+      if (otherHour < currentHour) {
+        $(this).addClass ('.past');
+      } else if (otherHour === currentHour) {
+        $(this).addClass('.present')
+        $(this).removeClass('.past')
+      } else {
+        $(this).addClass('.future')
+        $(this).removeClass('.past')
+        $(this).removeClass('.present')
+      }
+    })
+  }
+  workHour()
 
-      $('.time-block').each(function(){
-        var otherHour = parseInt($(this).attr('id').split('-')[1]);
 
-        if (otherHour < currentHour) {
-          $(this).addClass ('.past');
-        } else if (otherHour === currentHour) {
-          $(this).addClass('.present')
-          $(this).removeClass('.past')
-        } else {
-          $(this).addClass('.future')
-          $(this).removeClass('.past')
-          $(this).removeClass('.present')
-        }
-      })
-    }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-    $('#hour-9.description').val(localStorage.getItem('hour-9'));
-    $('#hour-10.description').val(localStorage.getItem('hour-10'));
-    $('#hour-11.description').val(localStorage.getItem('hour-11'));
-    $('#hour-12.description').val(localStorage.getItem('hour-12'));
-    $('#hour-13.description').val(localStorage.getItem('hour-13'));
-    $('#hour-14.description').val(localStorage.getItem('hour-14'));
-    $('#hour-15.description').val(localStorage.getItem('hour-15'));
-    $('#hour-16.description').val(localStorage.getItem('hour-16'));
-    $('#hour-17.description').val(localStorage.getItem('hour-17'));
-
+  document.querySelector('#hour-9 .description').value = localStorage.getItem('hour-9');
+  document.querySelector('#hour-10.description').value = localStorage.getItem('hour10');
+  document.querySelector('#hour-11 .description').value = localStorage.getItem('hour_11');
+  document.querySelector('#hour12.description').value = localStorage.getItem('hour-12');
+  document.querySelector('#hour-13.description').value = localStorage.getItem('hour-13');
+  document.querySelector('#hour-14 .description').value = localStorage.getItem('hour14');
+  document.querySelector('#hour_15.description').value = localStorage.getItem('hour-15');
+  document.querySelector('#hour-16 .description').value = localStorage.getItem('hour_16');
+  document.querySelector('#hour17.description').value = localStorage.getItem('hour-17');
+  
   // TODO: Add code to display the current date in the header of the page.
   function displayTime () {
     var todaysDate = dayjs().format('dddd MMMM DD, YYYY');
     currentDayEl.text(todaysDate);
   }
-  
   displayTime();
   setInterval(displayTime);
   
-  //printProjectData();
 });
 
-//hourUpdater()
