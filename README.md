06e113013b0c1b09a54ceb4447fde899ac7e881a
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-03-26 14:51:04 -0700

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# -Work-Day-Scheduler
+# Work Day Scheduler
+
+This project is a Work Day Scheduler. It is a simple and intuitive web-based scheduling application that allows users to plan their workday. 
+
+![portfolio demo](./assets/img/Screenshot%202023-03-26%20at%202.43.27%20PM.png)
+
+# Features
+
+Hourly time blocks from 9 AM to 5 PM
+Color-coded time blocks indicating past, present, and future events
+Local storage for persistence of user data
+Interactive input fields for easy editing and saving of tasks
+
+# Getting Started 
+To view my Work Day Scheduler visit "".
+
+To use the scheduler navigate to the desired time block by scrolling vertically. Click on the input field corresponding to the time block, enter the task description, and hit the save button. Your tasks will persist even when the page is reloaded, thanks to the local storage functionality.
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,25 +2,27 @@
 var currentDayEl = $('#currentDay')
 
 $(document).ready(function () {
-/* This is a function that is saving the text that is inputted into the text area. */
+
+  /* This is a function that is saving the text that is inputted into the text area. */
   $('.saveBtn').on('click', function(){
     var text = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
     
- /* This is a function that is saving the text that is inputted into the text area. */
+    /* This is a function that is saving the text that is inputted into the text area. */
     localStorage.setItem(time, text);
     
-/* This is a function that is adding a class to the time block depending on the time of day. */
+    /* This is a function that is adding a class to the time block depending on the time of day. */
     $('notification').addClass('show');
     
   })
+  
   /**
    * This function is adding a class to the time block depending on the time of day
    */
   function workHour () {
     var currentHour = dayjs().hour();
     
-   /* This is a function that is adding a class to the time block depending on the time of day. */
+    /* This is a function that is adding a class to the time block depending on the time of day. */
     $('.time-block').each(function(){
       var otherHour = parseInt($(this).attr('id').split('-')[1]);
       
@@ -37,31 +39,29 @@ $(document).ready(function () {
       }
     })
   }
+  
   workHour()
 
-
-
- /* This is a function that is saving the text that is inputted into the text area. */
+  /* This is a function that is saving the text that is inputted into the text area. */
   document.querySelector('#hour-9 .description').value = localStorage.getItem('hour-9');
-  document.querySelector('#hour-10.description').value = localStorage.getItem('hour10');
-  document.querySelector('#hour-11 .description').value = localStorage.getItem('hour_11');
-  document.querySelector('#hour12.description').value = localStorage.getItem('hour-12');
-  document.querySelector('#hour-13.description').value = localStorage.getItem('hour-13');
-  document.querySelector('#hour-14 .description').value = localStorage.getItem('hour14');
-  document.querySelector('#hour_15.description').value = localStorage.getItem('hour-15');
-  document.querySelector('#hour-16 .description').value = localStorage.getItem('hour_16');
-  document.querySelector('#hour17.description').value = localStorage.getItem('hour-17');
+  document.querySelector('#hour-10 .description').value = localStorage.getItem('hour-10');
+  document.querySelector('#hour-11 .description').value = localStorage.getItem('hour-11');
+  document.querySelector('#hour-12 .description').value = localStorage.getItem('hour-12');
+  document.querySelector('#hour-13 .description').value = localStorage.getItem('hour-13');
+  document.querySelector('#hour-14 .description').value = localStorage.getItem('hour-14');
+  document.querySelector('#hour-15 .description').value = localStorage.getItem('hour-15');
+  document.querySelector('#hour-16 .description').value = localStorage.getItem('hour-16');
+  document.querySelector('#hour-17 .description').value = localStorage.getItem('hour-17');
   
-
-/**
- * The above function displays the current date and time in the header.
- */
+  /**
+   * The above function displays the current date and time in the header.
+   */
   function displayTime () {
-    var todaysDate = dayjs().format('dddd MMMM DD, YYYY');
+    var todaysDate = dayjs().format('dddd, MMMM DD, YYYY, h:mm:ss A');
     currentDayEl.text(todaysDate);
   }
+  
   displayTime();
-  setInterval(displayTime);
+  setInterval(displayTime, 1000);
   
 });
-
